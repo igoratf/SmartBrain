8318540e1e717f1bdcab9b9ab8a21905dcc67c36
igoratf <igor.atf@gmail.com>
2019-01-15 15:53:46 -0300

    add face recognition api

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -3,7 +3,9 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 
 
@@ -13,13 +15,41 @@ const particlesOptions = {
       value: 150,
       density: {
         enable: true,
-        value_area: 800
+        value_area: 1000
       }
     }
   }
 };
 
+const app = new Clarifai.App({
+  apiKey: 'e818b2d0d9e94de880adf296a3184e56'
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+
+  }
+
+  onSubmit = () => {
+    console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,8 +58,9 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm onInputChange={this.onInputChange}
+                       onSubmit={this.onSubmit}/>
+        <FaceRecognition />
       </div>
     );
   }
---
+++ smartbrain/src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+   return (
+      <div className="center">
+         <img alt='image' src="https://samples.clarifai.com/face-det.jpg"></img>
+      </div>
+   );
+}
+
+
+export default FaceRecognition;
