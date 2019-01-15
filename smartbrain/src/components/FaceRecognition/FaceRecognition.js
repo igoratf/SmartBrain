46e8089e5c982ac910764f26de5068a147eb15ae
igoratf <igor.atf@gmail.com>
2019-01-15 16:19:16 -0300

    resize image

--- smartbrain/src/components/FaceRecognition/FaceRecognition.js
+++ smartbrain/src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({ imageUrl }) => {
    return (
-      <div className="center">
-         <img alt='' src={imageUrl}></img>
+      <div className="center ma">
+         <div className="absolute mt2">
+            <img alt='' src={imageUrl} width="500px" height="auto"></img>
+         </div>
       </div>
    );
 }
