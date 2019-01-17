ef20866da66ec16d89b066fa865d73f2aebe6964
igoratf <igor.atf@gmail.com>
2019-01-17 17:12:53 -0300

    add cursor to register button

--- smartbrain/src/components/SignIn/SignIn.js
+++ smartbrain/src/components/SignIn/SignIn.js
@@ -26,7 +26,7 @@ const SignIn = ({onRouteChange}) => {
          <div className="lh-copy mt3">
             <p 
             onClick={() => onRouteChange('register')}
-            className="f6 link dim black db">Register</p>
+            className="f6 link dim black db pointer">Register</p>
          </div>
          </form>
       </main>
