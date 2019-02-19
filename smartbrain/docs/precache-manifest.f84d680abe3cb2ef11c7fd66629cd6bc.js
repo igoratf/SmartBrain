3aebc3839366f7df8a95c82f8a3bb3ffead72307
igoratf <igor.atf@gmail.com>
2019-02-19 01:21:10 -0300

    prepare deploy

---
+++ smartbrain/docs/precache-manifest.f84d680abe3cb2ef11c7fd66629cd6bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa9100d9b186c17b3d2a",
+    "url": "/static/js/main.fa9100d9.chunk.js"
+  },
+  {
+    "revision": "8696c4232a1e71d7c965",
+    "url": "/static/js/1.8696c423.chunk.js"
+  },
+  {
+    "revision": "fa9100d9b186c17b3d2a",
+    "url": "/static/css/main.edeec3d0.chunk.css"
+  },
+  {
+    "revision": "8696c4232a1e71d7c965",
+    "url": "/static/css/1.0d00dc2b.chunk.css"
+  },
+  {
+    "revision": "98ecb78e70373f84e7dc8c3e539efb8a",
+    "url": "/index.html"
+  }
+];
